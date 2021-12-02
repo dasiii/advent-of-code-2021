1a39f2e24702ba27b7898ddb9d7680b4af097a0d
Daniel Stewart <contactdasiii@gmail.com>
2021-12-01 19:52:06 -0500

    types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('TypeScript Eslint Prettier Starter Template!');
+import fs from 'fs';
+console.log('start...');
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
