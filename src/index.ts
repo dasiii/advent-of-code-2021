e5ec6c45bd63049554dccbe827a2f06530f18d63
daniel stewart <contactdasiii@gmail.com>
2021-12-04 20:44:16 -0500

    refactor for part 2, part 2 mostly working

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ console.log('start...');
 interface BingoSetup {
   numbers: Array<number>;
   boards: Array<Array<Array<[number, boolean]>>>;
+  firstOrLast?: Board;
 }
 interface WinningBoard {
   board: Array<Array<[number, boolean]>>;
   winningNumber: number;
+  rank: number;
+}
+
+enum Board {
+  First,
+  Last,
 }
 
 const setupGame = function (inputLines: Array<string>): BingoSetup {
@@ -40,7 +47,11 @@ const setupGame = function (inputLines: Array<string>): BingoSetup {
   };
 };
 
-const getFirstWinningBoard = function (setup: BingoSetup): WinningBoard {
+const getFirstWinningBoards = function (
+  setup: BingoSetup
+): Array<WinningBoard> {
+  const winningBoards: Array<WinningBoard> = [];
+  let rank = 0;
   for (const num of setup.numbers) {
     // mark cells as true for the number drawn
     for (const board of setup.boards) {
@@ -51,11 +62,9 @@ const getFirstWinningBoard = function (setup: BingoSetup): WinningBoard {
       }
     }
 
-    // check if there is a winner
-    // check rows
-    // check columns
     for (const board of setup.boards) {
       const columns: Array<Array<[number, boolean]>> = [];
+
       for (let r = 0; r < board.length; r++) {
         for (let x = 0; x < board[r].length; x++) {
           if (!columns[x]) columns[x] = [];
@@ -66,24 +75,33 @@ const getFirstWinningBoard = function (setup: BingoSetup): WinningBoard {
       // check rows
       for (const row of board) {
         if (row.filter((c) => c[1] !== true).length === 0) {
-          return {
-            board: board,
-            winningNumber: num,
-          };
+          if (!winningBoards.some((b) => b.board == board)) {
+            winningBoards.push({
+              board: board,
+              winningNumber: num,
+              rank: ++rank,
+            });
+            if (setup.firstOrLast == Board.First) return winningBoards;
+          }
         }
       }
 
       // check columns
       for (const column of columns) {
         if (column.filter((c) => c[1] !== true).length === 0) {
-          return {
-            board: board,
-            winningNumber: num,
-          };
+          if (!winningBoards.some((b) => b.board == board)) {
+            winningBoards.push({
+              board: board,
+              winningNumber: num,
+              rank: ++rank,
+            });
+            if (setup.firstOrLast == Board.First) return winningBoards;
+          }
         }
       }
     }
   }
+  return winningBoards;
 };
 
 // Part one
@@ -94,7 +112,12 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
   }
 
   const setup = setupGame(data.split('\n'));
-  const firstWinningBoard = getFirstWinningBoard(setup);
+  setup.firstOrLast = Board.First;
+  const firstWinningBoard = getFirstWinningBoards(setup)?.[0];
+  if (!firstWinningBoard) {
+    console.log('No winning boards...');
+    return;
+  }
   const unmarkedNumbersSum = firstWinningBoard.board
     .flat()
     .filter((i) => !i[1])
@@ -107,3 +130,35 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     unmarkedNumbersSum * firstWinningBoard.winningNumber
   );
 });
+
+// Part two
+// fs.readFile('./input.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+
+//   const setup = setupGame(data.split('\n'));
+//   const winningBoards = getFirstWinningBoards(setup);
+//   if (!winningBoards.length) {
+//     console.log('No winning boards...');
+//     return;
+//   }
+
+//   const lastWinningBoard = winningBoards.reduce((acc, current) =>
+//     acc.rank > current.rank ? acc : current
+//   );
+//   const unmarkedNumbersSum = lastWinningBoard.board
+//     .flat()
+//     .filter((i) => {
+//       return !i[1];
+//     })
+//     .map((i) => i[0]);
+//   // .reduce((acc, current) => {
+//   //   return acc + current;
+//   // }, 0);
+//   // console.log(
+//   //   'Answer two: ',
+//   //   unmarkedNumbersSum * lastWinningBoard.winningNumber
+//   // );
+// });
